c43697a9a0dbf8aa322054bd0f4fda91626eda0f
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-17 09:54:18 +0000

    Formatted document at script.js

--- js/script.js
+++ js/script.js
@@ -2,22 +2,22 @@
 const NUMPRIMES = 10000;
 const PRIMES = Array(NUMPRIMES).fill(false);
 (function initPrimes(num) {
-    const upper = Math.floor((num-1)/2);
+    const upper = Math.floor((num - 1) / 2);
     const isPrime = Array(upper).fill(true);
-    const sqrtUpper = Math.floor((Math.sqrt(num) - 1)/2);
-    for (let i=0;i<=sqrtUpper;i++) {
+    const sqrtUpper = Math.floor((Math.sqrt(num) - 1) / 2);
+    for (let i = 0; i <= sqrtUpper; i++) {
         if (isPrime[i]) {
             const prime = 2 * i + 3;
             PRIMES[prime] = true;
             const primeSquaredIndex = 2 * i ** 2 + 6 * i + 3;
-            for (let j=primeSquaredIndex;j<upper;j+=prime) {
+            for (let j = primeSquaredIndex; j < upper; j += prime) {
                 isPrime[j] = false;
             }
         }
     }
 
-    for (let i=sqrtUpper + 1;i<upper;i++) {
-        if (isPrime[i]) PRIMES[2*i+3] = true;
+    for (let i = sqrtUpper + 1; i < upper; i++) {
+        if (isPrime[i]) PRIMES[2 * i + 3] = true;
     }
 })(NUMPRIMES);
 
@@ -51,10 +51,10 @@ function isPermutation(base, test) {
 function primePermutations() {
     const STEP = 3330, STEP2 = 6660;
     const upperBound = 9999 - STEP2;
-    for (let i=1235;i<upperBound;i+=2) {
-        if (i===1487) continue;
-        if (isPrime(i) && isPrime(i+STEP) && isPrime(i+STEP2) && isPermutation(i,i+STEP) && isPermutation(i,i+STEP2)) {
-            return parseInt(i.toString() + (i+STEP).toString() + (i+STEP2).toString());
+    for (let i = 1235; i < upperBound; i += 2) {
+        if (i === 1487) continue;
+        if (isPrime(i) && isPrime(i + STEP) && isPrime(i + STEP2) && isPermutation(i, i + STEP) && isPermutation(i, i + STEP2)) {
+            return parseInt(i.toString() + (i + STEP).toString() + (i + STEP2).toString());
         }
     }
 }
