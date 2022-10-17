9ff31d50ea67337f0aebb5957cbe814fbc389e39
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-17 09:36:45 +0000

    Updated initPrimes function at script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,23 @@
 const NUMPRIMES = 10000;
 const PRIMES = Array(NUMPRIMES).fill(false);
 (function initPrimes(num) {
-    
+    const upper = Math.floor((num-1)/2);
+    const isPrime = Array(upper).fill(true);
+    const sqrtUpper = Math.floor((Math.sqrt(num) - 1)/2);
+    for (let i=0;i<=sqrtUpper;i++) {
+        if (isPrime[i]) {
+            const prime = 2 * i + 3;
+            PRIMES[prime] = true;
+            const primeSquaredIndex = 2 * i ** 2 + 6 * i + 3;
+            for (let j=primeSquaredIndex;j<upper;j+=prime) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    for (let i=sqrtUpper + 1;i<upper;i++) {
+        if (isPrime[i]) PRIMES[2*i+3] = true;
+    }
 })(NUMPRIMES);
 
 
