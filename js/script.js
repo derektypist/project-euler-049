2ed3b9324053dcddf577c2a1e113cba04851b3aa
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-10-17 09:30:02 +0000

    Added initPrimes function at script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// Initialise Prime Number List
+const NUMPRIMES = 10000;
+const PRIMES = Array(NUMPRIMES).fill(false);
+(function initPrimes(num) {
+    
+})(NUMPRIMES);
+
+
+
+
+
+
 // Function to Show Solution
 function showSolution() {
     // Set Up Variable
